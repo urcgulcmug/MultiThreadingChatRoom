30b8e3c0c2604229d2ea1c228284f543e7fa5c32
urcgulcmug <105814753+urcgulcmug@users.noreply.github.com>
2024-03-10 19:48:08 +0500

    Update client.py

--- client.py
+++ client.py
@@ -1,84 +1,49 @@
-from threading import Thread
 import socket
+import threading
 
-clients = dict()
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+IP = input('Enter IP of chat room(or "localhost"): ')
+PORT = int(input('Enter the port of chat room: '))
 try:
-    ip_choice = int(input('Local-network IP adress or localhost(1/2): '))
-    if ip_choice == 1:
-        IP = socket.gethostbyname(socket.gethostname())
-        print(f'Your ip in local network is {IP}')
-    else:
-        IP = 'localhost'
-        print('You ip is 127.0.0.1')
-    PORT = int(input('Enter port: '))
-    server.bind((IP, PORT))
-    server.listen(5)
-    print('You started listening to ports on incoming connections')
+
+    client.connect((IP, PORT))
 except Exception as e:
+    client.close()
     print('Invalid IP or port:', e)
 
-def accept_connection():
-    global s
-    s = 0
+def recv_message(client):
     while True:
         try:
-            client, addr = server.accept()
-            print('Connected by', addr)
-            clients[client] = addr
-            print(clients)
-            s += 1
-            t3 = Thread(target=recv_from_client, args=(client, addr, len(clients.keys())))
-            t3.start()
+            t2 = threading.Thread(target=send_message, args=(client,))
+            t2.start()
+            data = client.recv(1024)
+            print('\nMessage from server:', data.decode())
+
+            if data.decode() == 'q':
+                break
         except Exception as e:
             print(e)
-            server.close()
             client.close()
             return
 
-def recv_from_client(client, addr, le):
-    global s
+def send_message(client):
     while True:
         try:
-            data = client.recv(4096)
-            datt = f'\nMessage from client({addr}): {data.decode()}'.encode()
-            print(datt.decode())
-            for i in clients.keys():
-                i.send(datt)
-            Thread(target=send_message, args=(client, s)).start()
-            s = 0
+            message = input('Enter message for server(or "quit"): ')
+            if message == 'quit':
+                client.close()
+                break
+            client.send(message.encode())
         except Exception as e:
             print(e)
-            server.close()
             client.close()
             return
-
-def send_message(client, arg):
-    if arg == 1:
-        client.send(b'You are connected!')
-
-    while True:
-        try:
-            response = input('Enter your message: ')
-            for i in clients.keys():
-                i.send(response.encode())
-            if response == 'quit':
-                server.close()
-                return
-        except Exception as e:
-            print(e)
-            server.close()
-            client.close()
-            return
-
-
-
 
 def event_loop():
-    t2 = Thread(target=accept_connection)
-
-    t2.start()
-    t2.join()
+    t1 = threading.Thread(target=recv_message, args=(client,))
+    t1.start()
+    t1.join()
 
 if __name__ == '__main__':
     event_loop()
+
